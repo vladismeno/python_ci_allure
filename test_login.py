f13a7845f3dff0551625560c1917cf6e64b493db
vladismeno <vladismeno@gmail.com>
2024-05-09 15:23:20 -0700

    New Project

--- test_login.py
+++ test_login.py
@@ -10,9 +10,9 @@ def test_login_passed():
     s('//*[@id="login_button_container"]/div/form/h3').should(be.visible)
 
 
-def test_login_failed():
-    browser.open('https://www.saucedemo.com/v1/')
-    s('//*[@id="user-name"]').type('standard_user')
-    s('//*[@id="password"]').type('secret_sauce')
-    s('//*[@id="login-button"]').click()
-    s('//*[@id="login_button_container"]/div/form/h3').should(be.visible)
+# def test_login_failed():
+#     browser.open('https://www.saucedemo.com/v1/')
+#     s('//*[@id="user-name"]').type('standard_user')
+#     s('//*[@id="password"]').type('secret_sauce')
+#     s('//*[@id="login-button"]').click()
+#     s('//*[@id="login_button_container"]/div/form/h3').should(be.visible)
